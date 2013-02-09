b0ab6d9613bd9970d602b619152ceb78f68f5fa1
Brian Mego <brianmego@gmail.com>
2013-02-09 10:52:45 -0600

    Added databindings to slots

--- Views/SolveGridView.xaml.cs
+++ Views/SolveGridView.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System.Windows.Controls;
 using System.Windows;
+using Sudoku_Solver.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Windows.Data;
 
 namespace Sudoku_Solver.Views
 {
@@ -8,14 +12,21 @@ namespace Sudoku_Solver.Views
     /// </summary>
     public partial class SolveGridView : UserControl
     {
+        SolveGridViewModel ViewModel = new SolveGridViewModel();
         public SolveGridView()
         {
             InitializeComponent();
-            DrawGameBoard(9);
+            //Gotta set the viewmodel in the codebehind because my brain won't give me any other options for dynamically setting the size of the board, sorry.
+            this.DataContext = ViewModel;
+
+            DrawGameBoard(ViewModel.SlotList);
+
+            
         }
 
-        private void DrawGameBoard(int desiredSlots)
+        private void DrawGameBoard(List<Models.Slot> slotList)
         {
+            int desiredSlots = (int)Math.Sqrt(slotList.Count);
             RowDefinition[] rowDefinitions = new RowDefinition[desiredSlots];
             ColumnDefinition[] columnDefinitions = new ColumnDefinition[desiredSlots];
 
@@ -27,18 +38,18 @@ namespace Sudoku_Solver.Views
                 grdGameBoard.RowDefinitions.Add(rowDefinitions[i]);
                 grdGameBoard.ColumnDefinitions.Add(columnDefinitions[i]);
             }
-            for (int i = 0; i < grdGameBoard.RowDefinitions.Count; i++)
+            foreach (Models.Slot slot in slotList)
             {
-                for (int j = 0; j < grdGameBoard.ColumnDefinitions.Count; j++)
-                {
-                    var slot = new TextBox();
-                    slot.Margin = new Thickness(5);
-                    slot.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Center;
-                    slot.VerticalContentAlignment = System.Windows.VerticalAlignment.Center;
-                    Grid.SetRow(slot, i);
-                    Grid.SetColumn(slot, j);
-                    grdGameBoard.Children.Add(slot);
-                }
+                var textBox = new TextBox();
+                textBox.Margin = new Thickness(5);
+                textBox.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Center;
+                textBox.VerticalContentAlignment = System.Windows.VerticalAlignment.Center;
+                Grid.SetRow(textBox, slot.Row);
+                Grid.SetColumn(textBox, slot.Column);
+                Binding myBinding = new Binding("Value");
+                myBinding.Source = slot;
+                textBox.SetBinding(TextBox.TextProperty, myBinding);
+                grdGameBoard.Children.Add(textBox);
             }
         }
     }
